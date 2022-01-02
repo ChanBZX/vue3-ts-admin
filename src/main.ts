8d35ff16ffeaae642a3833612ef229dd23610082
VEZON <wl885683@163.com>
2022-01-03 00:34:49 +0800

    vur-router config

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,29 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import { router } from './router'
+import './assets/css/index.css'
 
-import hcRequest from './service/index'
+// import hcRequest from './service/index'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
 
-interface DataType {
-  data: any
-  returnCode: string
-  success: boolean
-}
+// interface DataType {
+//   data: any
+//   returnCode: string
+//   success: boolean
+// }
 
 
 
-hcRequest.get<DataType>({
-  url: '/home/multidata',
-}).then((res) => {
-  console.log(res.data);
-  console.log(res.returnCode);
-  console.log(res.success);
-})
+// hcRequest.get<DataType>({
+//   url: '/home/multidata',
+// }).then((res) => {
+//   console.log(res.data);
+//   console.log(res.returnCode);
+//   console.log(res.success);
+// })
